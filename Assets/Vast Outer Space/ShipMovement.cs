6326b0630d8088368e625e1dcc49eef025cb5314
elsehow <yes@cosmopol.is>
2016-11-03 12:02:27 -0700

    first commit

---
+++ Assets/Vast Outer Space/ShipMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipMovement : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate ()
+	{
+		float forceNorthSouth = Input.GetAxis ("NorthSouth");
+		float forceEastWest = Input.GetAxis ("EastWest");
+		float forceUpDown = Input.GetAxis ("UpDown");
+
+		Vector3 movement = new Vector3 (forceEastWest, forceUpDown, forceNorthSouth);
+
+		rb.AddForce(movement);
+	}
+}
